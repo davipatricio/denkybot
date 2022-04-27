c1a6b42e8fa98a82d2bbbe189ba33e43573362a9
davipatricio <davipatricio2006@gmail.com>
2022-04-27 19:33:01 -0300

    remove dbots & start posting manually

--- src/bot/tasks/auto-post-stats.ts
+++ src/bot/tasks/auto-post-stats.ts
@@ -1,9 +1,9 @@
-import dbots from 'dbots';
 import { Task } from '../../structures/Task';
 import type { DenkyClient } from '../../types/Client';
+import { Poster } from '../utils/Poster';
 
 export default class ExampleTask extends Task {
-  poster: dbots.Poster;
+  poster: Poster;
   constructor() {
     super();
     this.name = 'Post bot stats automatically';
@@ -11,26 +11,11 @@ export default class ExampleTask extends Task {
     this.interval = null;
   }
 
-  override run(client: DenkyClient): Promise<any> | any {
+  override run(client: DenkyClient) {
     if (!global.IS_MAIN_PROCESS) return;
     console.log(`[DENKY] Running task ${this.name} now!`);
-    if (!this.poster)
-      this.poster = new dbots.Poster({
-        client,
-        apiKeys: {
-          discordbotsgg: process.env.BOTSGG_STATS_KEY as string,
-          bladebotlist: process.env.BLADELIST_STATS_KEY as string,
-          discordbotlist: process.env.DISCORDBOTLIST_STATS_KEY as string,
-          discords: process.env.DISCORDS_STATS_KEY as string,
-          disforge: process.env.DISFORGE_STATS_KEY as string,
-          fateslist: process.env.FATESLIST_STATS_KEY as string,
-          topgg: process.env.BOTSGG_STATS_KEY as string,
-        },
-        clientLibrary: 'discord.js',
-      });
+    if (!this.poster) this.poster = new Poster(client);
 
-    this.poster.post().catch(err => {
-      console.log(`Failed to post bot stats: ${err}`);
-    });
+    this.poster.post().catch(err => console.log(`Failed to post bot stats: ${err}`));
   }
 }
---
+++ src/bot/utils/Poster.ts
@@ -0,0 +1,182 @@
+import type { DenkyClient } from '../../types/Client';
+
+export class Poster {
+  client: DenkyClient;
+  constructor(client: DenkyClient) {
+    this.client = client;
+  }
+
+  async post() {
+    if (!global.IS_MAIN_PROCESS) return;
+
+    const serverCount = (await this.client.shard?.fetchClientValues('guilds.cache.size')) as number[];
+    const totalGuilds = serverCount.reduce((a, b) => a + b, 0);
+
+    await this.#postStatsToTopGG({ totalGuilds }).catch(console.log);
+    await this.#postStatsToDiscords({ totalGuilds }).catch(console.log);
+    await this.#postStatsToDisforge({ totalGuilds }).catch(console.log);
+    await this.#postStatsToBladeList({ totalGuilds }).catch(console.log);
+    await this.#postStatsToFatesList({ totalGuilds }).catch(console.log);
+    await this.#postStatsToDiscordBots({ totalGuilds }).catch(console.log);
+    await this.#postStatsToDiscordBotList({ totalGuilds }).catch(console.log);
+  }
+
+  async #postStatsToTopGG({ totalGuilds }: { totalGuilds: number }) {
+    const Authorization = process.env.TOPGG_STATS_KEY;
+    if (!Authorization) {
+      console.log(`[DENKY] No top.gg API key found! Ignoring...`);
+      return false;
+    }
+
+    const request = await fetch(`https://top.gg/api/bots/${this.client.user?.id}/stats`, {
+      method: 'POST',
+      headers: {
+        Authorization,
+      },
+      body: JSON.stringify({
+        server_count: totalGuilds,
+        shard_count: process.env.SHARD_COUNT ?? 1,
+      }),
+    });
+
+    if (request.status >= 400) throw new Error('Failed to post stats to top.gg!');
+    console.log('Posted stats to top.gg successfully!');
+    return true;
+  }
+
+  async #postStatsToDiscordBots({ totalGuilds }: { totalGuilds: number }) {
+    const Authorization = process.env.BOTSGG_STATS_KEY;
+    if (!Authorization) {
+      console.log(`[DENKY] No bots.gg API key found! Ignoring...`);
+      return false;
+    }
+
+    const request = await fetch(`https://discord.bots.gg/api/v1/bots/${this.client.user?.id}/stats`, {
+      method: 'POST',
+      headers: {
+        Authorization,
+      },
+      body: JSON.stringify({
+        guildCount: totalGuilds,
+        shardCount: process.env.SHARD_COUNT ?? 1,
+      }),
+    });
+
+    if (request.status >= 400) throw new Error('Failed to post stats to bots.gg!');
+    console.log('Posted stats to bots.gg successfully!');
+    return true;
+  }
+
+  async #postStatsToDiscords({ totalGuilds }: { totalGuilds: number }) {
+    const Authorization = process.env.DISCORDS_STATS_KEY;
+    if (!Authorization) {
+      console.log(`[DENKY] No discords.com API key found! Ignoring...`);
+      return false;
+    }
+
+    const request = await fetch(`https://discords.com/api/bots/${this.client.user?.id}/stats`, {
+      method: 'POST',
+      headers: {
+        Authorization,
+      },
+      body: JSON.stringify({
+        server_count: totalGuilds,
+      }),
+    });
+
+    if (request.status >= 400) throw new Error('Failed to post stats to discords!');
+    console.log('Posted stats to discords.com successfully!');
+    return true;
+  }
+
+  async #postStatsToDisforge({ totalGuilds }: { totalGuilds: number }) {
+    const Authorization = process.env.DISFORGE_STATS_KEY;
+    if (!Authorization) {
+      console.log(`[DENKY] No disforge.com API key found! Ignoring...`);
+      return false;
+    }
+
+    const request = await fetch(`https://disforge.com/api/botstats/${this.client.user?.id}`, {
+      method: 'POST',
+      headers: {
+        Authorization,
+      },
+      body: JSON.stringify({
+        servers: totalGuilds,
+      }),
+    });
+
+    if (request.status >= 400) throw new Error('Failed to post stats to disforge!');
+    console.log('Posted stats to disforge.com successfully!');
+    return true;
+  }
+
+  async #postStatsToDiscordBotList({ totalGuilds }: { totalGuilds: number }) {
+    const Authorization = process.env.DISCORDBOTLIST_STATS_KEY;
+    if (!Authorization) {
+      console.log(`[DENKY] No discordbotlist.com API key found! Ignoring...`);
+      return false;
+    }
+
+    const request = await fetch(`https://discordbotlist.com/api/v1/${this.client.user?.id}/stats`, {
+      method: 'POST',
+      headers: {
+        Authorization,
+      },
+      body: JSON.stringify({
+        guilds: totalGuilds,
+      }),
+    });
+
+    if (request.status >= 400) throw new Error('Failed to post stats to discordbotlist.com!');
+    console.log('Posted stats to discordbotlist.com successfully!');
+    return true;
+  }
+
+  async #postStatsToBladeList({ totalGuilds }: { totalGuilds: number }) {
+    const Authorization = process.env.BLADELIST_STATS_KEY;
+    if (!Authorization) {
+      console.log(`[DENKY] No bladelist.gg API key found! Ignoring...`);
+      return false;
+    }
+
+    const request = await fetch(`https://api.bladelist.gg/bots/${this.client.user?.id}`, {
+      method: 'POST',
+      headers: {
+        Authorization,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        server_count: totalGuilds,
+        shard_count: process.env.SHARD_COUNT ?? 1,
+      }),
+    });
+
+    if (request.status >= 400) throw new Error('Failed to post stats to bladelist.gg!');
+    console.log('Posted stats to bladelist.gg successfully!');
+    return true;
+  }
+
+  async #postStatsToFatesList({ totalGuilds }: { totalGuilds: number }) {
+    const Authorization = process.env.FATESLIST_STATS_KEY;
+    if (!Authorization) {
+      console.log(`[DENKY] No fateslist.xyz API key found! Ignoring...`);
+      return false;
+    }
+
+    const request = await fetch(`https://api.fateslist.xyz/bots/${this.client.user?.id}/stats`, {
+      method: 'POST',
+      headers: {
+        Authorization,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        guild_count: totalGuilds,
+      }),
+    });
+
+    if (request.status >= 400) throw new Error('Failed to post stats to fateslist.xyz!');
+    console.log('Posted stats to fateslist.xyz successfully!');
+    return true;
+  }
+}
