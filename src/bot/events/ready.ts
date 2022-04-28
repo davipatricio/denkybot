daff879ecf9bc21e293b2914cea4cb7870f34f6d
Nicolas Ribeiro <nicolas.ribeiro.s@outlook.com>
2022-04-28 15:32:46 -0300

    Update ready.ts

--- src/bot/events/ready.ts
+++ src/bot/events/ready.ts
@@ -9,6 +9,6 @@ export default class ReadyEvent extends Event {
   }
 
   override run(client: DenkyClient): Awaitable<any> {
-    console.log('✅ \x1b[34m[DENKY]\x1b[0m', `Shard ${client.shard?.ids[0]} connected.`);
+    console.log('✅ \x1b[34m[SHARDS]\x1b[0m', `Shard ${client.shard?.ids[0]} connected.`);
   }
 }
