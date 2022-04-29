842a61bb656ddc25a436bd45604be2a6f40965a5
davipatricio <davipatricio2006@gmail.com>
2022-04-29 19:41:07 -0300

    fix: afk not working

--- src/bot/commands/Utils/afk.ts
+++ src/bot/commands/Utils/afk.ts
@@ -60,7 +60,7 @@ export default class PingCommand extends Command {
 
   override async run({ t, interaction }: CommandRunOptions) {
     switch (interaction.options.getSubcommand()) {
-      case 'ativar': {
+      case 'on': {
         if (this.client.databases.afk.get(interaction.user.id)) {
           interaction.editReply(t('command:afk/alreadySet', interaction.user));
           break;
@@ -80,7 +80,7 @@ export default class PingCommand extends Command {
         break;
       }
 
-      case 'desativar': {
+      case 'off': {
         const data = this.client.databases.afk.get(interaction.user.id);
         if (!data) {
           interaction.editReply(t('command:afk/notAfk', interaction.user));
