8b0a55111829f279e6e8d30b1d3ea57975db356c
davipatricio <davipatricio2006@gmail.com>
2022-04-30 11:22:03 -0300

    feat: add shouldTryToPreventCrashes option

--- src/bot/events/ready.ts
+++ src/bot/events/ready.ts
@@ -11,6 +11,21 @@ export default class ReadyEvent extends Event {
   override async run(client: DenkyClient) {
     console.log('✅ \x1b[34m[DENKY]\x1b[0m', `Shard ${client.shard?.ids[0]} connected.`);
 
+    if (client.config.shouldTryToPreventCrashes) {
+      client.on('error', err => {
+        console.error('❌ \x1b[31m[DENKY]\x1b[0m', err);
+      });
+
+      process.on('unhandledRejection', err => {
+        console.error('❌ \x1b[31m[DENKY]\x1b[0m', err);
+      });
+
+      process.on('uncaughtException', err => {
+        console.error('❌ \x1b[31m[DENKY]\x1b[0m', err);
+      });
+    }
+
+    // Publish cached commands to Discord
     if (!global.IS_MAIN_PROCESS) return;
     const finalData: ChatInputApplicationCommandData[] = [];
     for (const command of client.commands.values()) finalData.push(command.options);
