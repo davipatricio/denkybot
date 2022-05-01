c3356ee8ac95ea283a166d9bb4df5bb7499bff9d
davipatricio <davipatricio2006@gmail.com>
2022-05-01 13:11:35 -0300

    improve recommendLocale & add formatLocale

--- src/bot/modules/Helpers.ts
+++ src/bot/modules/Helpers.ts
@@ -1,5 +1,6 @@
-import type { Locale } from 'discord.js';
+import { Locale } from 'discord.js';
 import type { DenkyClient } from '../../types/Client';
+import type { SupportedLocales } from '../managers/LanguageManager';
 
 export default class Helpers {
   /** The client that instantiated this module */
@@ -9,8 +10,25 @@ export default class Helpers {
     this.client = client;
   }
 
-  recommendLanguage(locale?: Locale) {
-    if (!locale) return this.client.config.defaultLanguage;
+  recommendLocale(locale?: Locale) {
+    if (!locale) return this.client.config.defaultLanguage as SupportedLocales;
+    let recommendedLocale = this.client.config.defaultLanguage as SupportedLocales;
+    switch (this.formatLocale(locale)) {
+      case Locale.EnglishUS:
+      case Locale.EnglishGB:
+        recommendedLocale = 'en_US';
+        break;
+      case Locale.PortugueseBR:
+        recommendedLocale = 'pt_BR';
+        break;
+      default:
+        recommendedLocale = this.client.config.defaultLanguage as SupportedLocales;
+        break;
+    }
+    return recommendedLocale;
+  }
+
+  formatLocale(locale: Locale) {
     return locale.replace('-', '_');
   }
 }
