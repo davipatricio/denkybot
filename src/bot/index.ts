04878655134c47a596929abee14b027ad41e27c4
davipatricio <davipatricio2006@gmail.com>
2022-05-01 20:16:08 -0300

    use sucrase instead of original typescript transpiler

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -3,7 +3,7 @@ import { config } from 'dotenv';
 import type { DenkyClient } from '../types/Client';
 import { Initializer } from './utils/Initializer';
 
-config({ path: '../../.env' });
+config({ path: '../.env' });
 
 // @ts-ignore
 const client: DenkyClient = new Client({
