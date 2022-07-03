27ae0672340de709bf7b91cc57cfb80cd4147da6
Davi Patricio <davipatricio@pm.me>
2022-07-03 10:22:40 -0300

    interactions: verify permissions through `appPermissions`

--- src/bot/events/interactionCreate.ts
+++ src/bot/events/interactionCreate.ts
@@ -73,7 +73,7 @@ export default class InteractionCreateEvent extends Event {
 
   static checkBotPermissions(interaction: ChatInputCommandInteraction, command: Command, t: CommandLocale): boolean {
     if (command.permissions.bot.length === 0) return true;
-    if (!interaction.guild?.members.me?.permissions.has(command.permissions.bot)) {
+    if (!interaction.appPermissions?.has(command.permissions.bot)) {
       const permissions = new PermissionsBitField(command.permissions.bot)
         .toArray()
         .map(p => t(`permissions:${p}`))
