87997b12504a58fe09d1d039229916d9599a612a
Nicolas Ribeiro <nicolas.ribeiro.s@outlook.com>
2022-07-06 13:15:36 -0300

    feat(DatabaseManager): add utility suggestion methods

--- src/bot/managers/DatabaseManager.ts
+++ src/bot/managers/DatabaseManager.ts
@@ -1,3 +1,58 @@
 import { PrismaClient } from '@prisma/client';
 
-export class DatabaseManager extends PrismaClient {}
+export interface SuggestionConfig {
+  guildId: string;
+  addReactions?: boolean;
+  categories?: string[];
+  cooldown?: number;
+  useThreads?: boolean;
+}
+
+export type FullSuggestionConfig = Required<SuggestionConfig>;
+
+export class DatabaseManager extends PrismaClient {
+  // #region Suggestion
+  createSuggestion(config: SuggestionConfig) {
+    return this.suggestion.create({
+      data: {
+        guildId: config.guildId,
+        addReactions: config.addReactions,
+        categories: config.categories,
+        cooldown: config.cooldown,
+        useThreads: config.useThreads
+      }
+    });
+  }
+
+  getSuggestion(guildId: string) {
+    return this.suggestion.findFirst({
+      where: {
+        guildId
+      }
+    });
+  }
+
+  deleteSuggestion(guildId: string) {
+    return this.suggestion.delete({
+      where: {
+        guildId
+      }
+    });
+  }
+
+  updateSuggestion(config: FullSuggestionConfig) {
+    return this.suggestion.update({
+      where: {
+        guildId: config.guildId
+      },
+      data: {
+        addReactions: config.addReactions,
+        categories: config.categories,
+        cooldown: config.cooldown,
+        useThreads: config.useThreads
+      }
+    });
+  }
+
+  // #endregion
+}
