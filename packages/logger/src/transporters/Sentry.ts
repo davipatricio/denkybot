773bb81d19d0a320a13fec7a0adc617d090645b3
davipatricio <davipatricio@pm.me>
2022-07-13 18:59:52 -0300

    fix(logger): remove `setImmediate` method

--- packages/logger/src/transporters/Sentry.ts
+++ packages/logger/src/transporters/Sentry.ts
@@ -6,17 +6,13 @@ export class SentryTransporter extends Transport {
   constructor(dsn: string) {
     super({ level: 'warn' });
 
-    if (!dsn) throw new TypeError('Missing Sentry dsn.');
-
     init({ dsn });
   }
 
   override log(info: LogEntry & { message: any }, callback: LogCallback) {
-    setImmediate(() => {
-      if (info.message instanceof Error) captureException(info.message);
-      else captureMessage(String(info.message));
-      this.emit('logged', info);
-    });
+    if (info.message instanceof Error) captureException(info.message);
+    else captureMessage(String(info.message));
+    this.emit('logged', info);
 
     callback();
   }
--- packages/logger/src/transporters/Webhook.ts
+++ packages/logger/src/transporters/Webhook.ts
@@ -8,26 +8,21 @@ export class WebhookTransporter extends Transport {
 
   constructor(url: string) {
     super({ level: 'error' });
-
-    if (!url) throw new TypeError('Missing webhook url.');
-
     this.webhook = new WebhookClient({ url });
   }
 
   override log(info: LogEntry, callback: LogCallback) {
-    setImmediate(async () => {
-      const embed = new EmbedBuilder()
-        .setColor('Purple')
-        .setAuthor({
-          name: info.level.includes('warn') ? '⚠ WARN' : '❌ ERROR'
-        })
-        .setDescription(`\`\`\`js\n${inspect(info.message, { depth: 0 })}\`\`\``)
-        .setFooter({ text: 'Denky Logger' })
-        .setTimestamp(info.timestamp);
+    const embed = new EmbedBuilder()
+      .setColor('Purple')
+      .setAuthor({
+        name: info.level.includes('warn') ? '⚠ WARN' : '❌ ERROR'
+      })
+      .setDescription(`\`\`\`js\n${inspect(info.message, { depth: 0 })}\`\`\``)
+      .setFooter({ text: 'Denky Logger' })
+      .setTimestamp(info.timestamp);
 
-      await this.webhook.send({ embeds: [embed] });
-      this.emit('logged', info);
-    });
+    this.webhook.send({ embeds: [embed] }).catch(() => {});
+    this.emit('logged', info);
 
     callback();
   }
