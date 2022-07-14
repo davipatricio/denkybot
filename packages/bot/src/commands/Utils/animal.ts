b47a45f01751e5ac5af4f7f71d16dae36a8f293b
davipatricio <davipatricio2006@gmail.com>
2022-07-14 22:59:15 +0000

    refactor(bot): merge animal subcommand into a single one

--- packages/bot/src/commands/Utils/animal.ts
+++ packages/bot/src/commands/Utils/animal.ts
@@ -1,4 +1,4 @@
-import { PermissionFlagsBits } from 'discord.js';
+import { EmbedBuilder, PermissionFlagsBits } from 'discord.js';
 import { Command, CommandRunOptions } from '../../structures/Command';
 import type { DenkyClient } from '../../types/Client';
 
@@ -15,35 +15,33 @@ export default class AnimalCommand extends Command {
     this.permissions = { bot: [PermissionFlagsBits.EmbedLinks] };
   }
 
-  override run({ t, interaction }: CommandRunOptions) {
-    switch (interaction.options.getSubcommand()) {
-      case 'panda':
-        this.client.commands.get('_animal_panda')?.run({ t, interaction });
-        break;
+  override async run({ t, interaction }: CommandRunOptions) {
+    const baseEmbed = new EmbedBuilder().setFooter({ text: interaction.user.tag, iconURL: interaction.user.displayAvatarURL() }).setTimestamp().setColor('Purple');
 
-      case 'dog':
-        this.client.commands.get('_animal_dog')?.run({ t, interaction });
-        break;
-
-      case 'koala':
-        this.client.commands.get('_animal_koala')?.run({ t, interaction });
-        break;
-
+    switch (interaction.options.getSubcommand(true)) {
       case 'bunny':
-        this.client.commands.get('_animal_bunny')?.run({ t, interaction });
+        baseEmbed.setTitle(t('command:animal/bunny')).setImage(await this.client.apis.animals.getRandomBunny());
         break;
-
       case 'cat':
-        this.client.commands.get('_animal_cat')?.run({ t, interaction });
+        baseEmbed.setTitle(t('commandNames:animal/cat')).setImage(await this.client.apis.animals.getRandomCat());
         break;
-
+      case 'dog':
+        baseEmbed.setTitle(t('command:animal/dog')).setImage(await this.client.apis.animals.getRandomCat());
+        break;
       case 'duck':
-        this.client.commands.get('_animal_duck')?.run({ t, interaction });
+        baseEmbed.setTitle(t('command:animal/duck')).setImage(await this.client.apis.animals.getRandomDuck());
         break;
-
       case 'foxy':
-        this.client.commands.get('_animal_foxy')?.run({ t, interaction });
+        baseEmbed.setTitle(t('command:animal/foxy')).setImage(await this.client.apis.animals.getRandomFoxy());
+        break;
+      case 'koala':
+        baseEmbed.setTitle(t('command:animal/koala')).setImage(await this.client.apis.animals.getRandomKoala());
+        break;
+      case 'panda':
+        baseEmbed.setTitle(t('command:animal/bunny')).setImage(await this.client.apis.animals.getRandomPanda());
         break;
     }
+
+    interaction.editReply({ embeds: [baseEmbed] });
   }
 }
