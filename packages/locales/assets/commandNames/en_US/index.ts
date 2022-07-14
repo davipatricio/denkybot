8f8c4786ebb4d871de5ddc01be496c501f2a1dd1
Davi Patricio <davipatricio2006@gmail.com>
2022-07-14 12:29:05 -0300

    New translations index.ts (English)

--- packages/locales/assets/commandNames/en_US/index.ts
+++ packages/locales/assets/commandNames/en_US/index.ts
@@ -1,99 +1,86 @@
-export default {
+export default ({
   // Ping
   ping: 'ping',
-
   // AFK
-  afk: 'afk on | off',
-  'afk/on': 'on',
-  'afk/off': 'off',
-  'afk/reason': 'reason',
-
+  afk: "afk on | off",
+  'afk/on': "on",
+  'afk/off': "off",
+  'afk/reason': "reason",
   // Poll
-  poll: 'poll',
-  'poll/create': 'create',
-  'poll/create/permanent': 'permanent',
-  'poll/create/option1': 'option1',
-  'poll/create/option2': 'option2',
-  'poll/create/option3': 'option3',
-  'poll/create/option4': 'option4',
-  'poll/create/option5': 'option5',
-  'poll/create/option6': 'option6',
-  'poll/create/option7': 'option7',
-  'poll/create/option8': 'option8',
-  'poll/create/option9': 'option9',
-
+  poll: "poll",
+  'poll/create': "create",
+  'poll/create/permanent': "permanent",
+  'poll/create/option1': "option1",
+  'poll/create/option2': "option2",
+  'poll/create/option3': "option3",
+  'poll/create/option4': "option4",
+  'poll/create/option5': "option5",
+  'poll/create/option6': "option6",
+  'poll/create/option7': "option7",
+  'poll/create/option8': "option8",
+  'poll/create/option9': "option9",
   // Help
-  help: 'help',
-
+  help: "help",
   // User
   user: 'user avatar | info | banner',
-  'user/info/user': 'user',
-  'user/avatar/user': 'user',
-  'user/banner/user': 'user',
-
+  'user/info/user': "user",
+  'user/avatar/user': "user",
+  'user/banner/user': "user",
   // Server
   server: 'server avatar | info | banner',
-
   // Bot
-  bot: 'bot info | convidar | votar',
-  'bot/invite': 'invite',
-  'bot/vote': 'vote',
-
+  bot: "bot info | convidar | votar",
+  'bot/invite': "invite",
+  'bot/vote': "vote",
   // Text
-  text: 'text',
-  'text/claps': 'claps',
-  'text/invert': 'invert',
-
+  text: "text",
+  'text/claps': "claps",
+  'text/invert': "invert",
   // Config
   config: 'config',
-  'config/suggestions': 'suggestions',
-
+  'config/suggestions': "suggestions",
   // Suggestions
-  suggestions: 'suggestions',
-  suggestion: 'suggestion',
-  'suggestion/send': 'send',
-  'suggestion/edit': 'edit',
-  'suggestion/accept': 'accept',
-  'suggestion/accept/id': 'id_suggestion',
-  'suggestion/accept/reason': 'reason',
-  'suggestion/deny': 'deny',
-  'suggestion/deny/id': 'id_suggestion',
-  'suggestion/deny/reason': 'reason',
-
+  suggestions: "suggestions",
+  suggestion: "suggestion",
+  'suggestion/send': "send",
+  'suggestion/edit': "edit",
+  'suggestion/accept': "accept",
+  'suggestion/accept/id': "id_suggestion",
+  'suggestion/accept/reason': "reason",
+  'suggestion/deny': "deny",
+  'suggestion/deny/id': "id_suggestion",
+  'suggestion/deny/reason': "reason",
   // Ban
   ban: 'ban',
-  'ban/user': 'user',
-  'ban/delete_messages': 'delete_messages',
-  'ban/delete_messages/day': 'day',
-  'ban/delete_messages/days': 'days',
-  'ban/reason': 'reason',
-
+  'ban/user': "user",
+  'ban/delete_messages': "delete_messages",
+  'ban/delete_messages/day': "day",
+  'ban/delete_messages/days': "days",
+  'ban/reason': "reason",
   // Wikipedia
-  'wikipedia/search': 'search',
-
+  wikipedia: 'wikipedia',
+  'wikipedia/search': "search",
   // Mute
-  mute: 'mute',
-  'mute/user': 'user',
-  'mute/time': 'time',
-  'mute/time/minute': 'minute',
-  'mute/time/minutes': 'minutes',
-  'mute/time/day': 'day',
-  'mute/time/days': 'days',
-  'mute/time/hour': 'hour',
-  'mute/time/hours': 'hours',
-  'mute/reason': 'reason',
-
+  mute: "mute",
+  'mute/user': "user",
+  'mute/time': "time",
+  'mute/time/minute': "minute",
+  'mute/time/minutes': "minutes",
+  'mute/time/day': "day",
+  'mute/time/days': "days",
+  'mute/time/hour': "hour",
+  'mute/time/hours': "hours",
+  'mute/reason': "reason",
   // Kick
-  kick: 'kick',
-  'kick/user': 'user',
-  'kick/reason': 'reason',
-
+  kick: "kick",
+  'kick/user': "user",
+  'kick/reason': "reason",
   // Animal
-  animal: 'animal | dog | koala | bunny | cat | duck | foxy',
-  'animal/dog': 'dog',
-  'animal/koala': 'koala',
-  'animal/bunny': 'bunny',
-  'animal/cat': 'cat',
-  'animal/duck': 'duck',
-  'animal/foxy': 'foxy'
-} as const;
+  animal: "animal | dog | koala | bunny | cat | duck | foxy",
+  'animal/dog': "dog",
+  'animal/koala': "koala",
+  'animal/bunny': "bunny",
+  'animal/cat': "cat",
+  'animal/duck': "duck",
+  'animal/foxy': "foxy"
+} as const);
