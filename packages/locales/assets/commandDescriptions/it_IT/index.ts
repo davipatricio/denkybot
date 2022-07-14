4b84ca9e4a7d5bb1a7f58ccea892db303a53b667
Davi Patricio <davipatricio2006@gmail.com>
2022-07-14 12:29:00 -0300

    New translations index.ts (Italian)

---
+++ packages/locales/assets/commandDescriptions/it_IT/index.ts
@@ -0,0 +1,88 @@
+export default ({
+  // Ping
+  ping: 'Informa o ping do Denky no seu servidor',
+  // AFK
+  afk: 'Defina um status AFK para exibir quando você for mencionado',
+  'afk/on': 'Defina um status AFK para exibir quando você for mencionado',
+  'afk/on/reason': 'Motivo por ficar ausente',
+  'afk/off': 'Desabilita seu modo AFK',
+  // Poll
+  poll: 'Realize enquetes com até 9 opções',
+  'poll/create': 'Cria uma enquete neste canal',
+  'poll/create/permanent': 'Esta enquete deverá ser permanente?',
+  'poll/create/option1': '1º opção da enquete',
+  'poll/create/option2': '2º opção da enquete',
+  'poll/create/option3': '3º opção da enquete',
+  'poll/create/option4': '4º opção da enquete',
+  'poll/create/option5': '5º opção da enquete',
+  'poll/create/option6': '6º opção da enquete',
+  'poll/create/option7': '7º opção da enquete',
+  'poll/create/option8': '8º opção da enquete',
+  'poll/create/option9': '9º opção da enquete',
+  // Help
+  help: 'Veja os comandos do bot',
+  // User
+  user: 'Veja informações sobre um usuário',
+  'user/avatar': 'Veja o avatar de um usuário',
+  'user/avatar/user': 'O usuário para ver o avatar',
+  'user/info': 'Veja informações sobre um usuário',
+  'user/info/user': 'O usuário para ver as informações',
+  'user/banner': 'Veja o banner de um usuário',
+  'user/banner/user': 'O usuário para ver o banner',
+  // Server
+  server: 'Veja informações sobre um servidor',
+  'server/info': 'Veja informações sobre um servidor',
+  'server/icon': 'Veja o ícone de um servidor',
+  'server/banner': 'Veja o banner de um servidor',
+  // Bot
+  bot: 'Veja informações sobre o bot',
+  'bot/invite': 'Convide o Denky para seu servidor',
+  'bot/vote': 'Informa o link para votar em mim',
+  // Text
+  text: 'Traduza ou edite texto em diferentes maneiras',
+  'text/text': 'O texto para editar',
+  'text/claps': 'Envie 👏 um 👏 texto 👏 com 👏 palmas',
+  'text/invert': 'Inverte um texto',
+  'text/emojify': 'Transforme um texto em emojis',
+  'text/vaporwave': 'Faça uma mensagem V A P O R W A V E',
+  // Config
+  config: 'Configure funcionalidades do Denky no seu servidor',
+  'config/suggestions': 'Configure o sistema de sugestões do Denky',
+  // Suggestions
+  suggestions: 'Envie sugestões para o servidor',
+  'suggestions/send': 'Envie sugestões para o servidor',
+  'suggestions/edit': 'Edite uma sugestão que você enviou anteriormente',
+  'suggestions/edit/id': 'ID da sua sugestão',
+  'suggestions/accept': 'Aceite uma sugestão que foi enviada',
+  'suggestions/accept/id': 'ID da sugestão para aceitar',
+  'suggestions/accept/reason': 'A razão para aceitar a sugestão',
+  'suggestions/deny': 'Recuse uma sugestão que foi enviada',
+  'suggestions/deny/id': 'ID da sugestão para recusar',
+  'suggestions/deny/reason': 'A razão para recusar a sugestão',
+  // Ban
+  ban: 'Bane um usuário do servidor',
+  'ban/user': 'O usuário para banir',
+  'ban/delete_messages': 'Se devo ou não excluir as mensagens do usuário banido',
+  'ban/reason': 'O motivo do banimento',
+  // Wikipedia
+  wikipedia: 'Veja informações sobre uma página da Wikipedia',
+  'wikipedia/search': 'Termo para buscar na Wikipedia',
+  // Mute
+  mute: 'Silencia um usuário do servidor',
+  'mute/user': 'O usuário para silenciar',
+  'mute/time': 'Tempo de silenciamento',
+  'mute/reason': 'O motivo do silenciamento',
+  // Kick
+  kick: 'Expulsa um usuário do servidor',
+  'kick/user': 'O usuário para expulsar',
+  'kick/reason': 'O motivo da expulsão',
+  // Animal
+  animal: 'Veja uma imagem aleatória de um animal',
+  'animal/panda': 'Veja uma imagem aleatória de um panda',
+  'animal/cat': 'Veja uma imagem aleatória de um gato',
+  'animal/dog': 'Veja uma imagem aleatória de um cachorro',
+  'animal/foxy': 'Veja uma imagem aleatória de um raposa',
+  'animal/bunny': 'Veja uma imagem aleatória de um coelho',
+  'animal/koala': 'Veja uma imagem aleatória de um coala',
+  'animal/duck': 'Veja uma imagem aleatória de um pato'
+} as const);
