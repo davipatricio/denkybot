a0e56504da88138a4a6b5ed75f48e6615978da85
Davi Patricio <davipatricio2006@gmail.com>
2022-07-14 13:07:54 -0300

    Update source file index.ts

--- packages/locales/assets/commandDescriptions/en_US/index.ts
+++ packages/locales/assets/commandDescriptions/en_US/index.ts
@@ -1,11 +1,13 @@
 export default {
   // Ping
   ping: 'Get the ping for Denky in your server',
+
   // AFK
   afk: 'Set an AFK status to display when you are mentioned',
   'afk/on': 'Set an AFK status to display when you are mentioned',
   'afk/on/reason': 'Reason for being AFK',
   'afk/off': 'Disables your AFK mode',
+
   // Poll
   poll: 'Create polls with up to 9 options',
   'poll/create': 'Creates a poll in this channel',
@@ -19,8 +21,10 @@ export default {
   'poll/create/option7': '7th poll option',
   'poll/create/option8': '8th poll option',
   'poll/create/option9': '9th poll option',
+
   // Help
   help: 'Shows the bot commands',
+
   // User
   user: 'Shows user information',
   'user/avatar': 'Shows the user avatar',
@@ -29,15 +33,18 @@ export default {
   'user/info/user': 'The user to show information for',
   'user/banner': 'Shows the user banner',
   'users/banner/user': 'The user to get the banner of',
+
   // Server
   server: 'Shows server information',
   'server/info': 'View information about a server',
   'server/icon': 'See a server icon',
   'server/banner': 'See a server banner',
+
   // Bot
   bot: 'Shows bot information',
   'bot/invite': 'Invite Denky to your server',
   'bot/vote': 'Informs the link to vote for me',
+
   // Text
   text: 'Translates or edits text in different ways',
   'text/text': 'Text to edit',
@@ -45,9 +52,11 @@ export default {
   'text/invert': 'Inverts a text',
   'text/emojify': 'Emojifies a text',
   'text/vaporwave': 'Creates a V A P O R W A V E text',
+
   // Config
   config: 'Configure Denky features on your server',
   'config/suggestions': "Configure Denky's suggestion system",
+
   // Suggestions
   suggestions: 'Send suggestions to the server',
   'suggestions/send': 'Send suggestions to the server',
@@ -59,23 +68,28 @@ export default {
   'suggestions/deny': 'Denies a suggestion that was previously submitted',
   'suggestions/deny/id': 'ID of the suggestion to deny',
   'suggestions/deny/reason': 'The reason for denying the suggestion',
+
   // Ban
   ban: 'Bans a user from the server',
   'ban/user': 'The user to ban',
   'ban/delete_messages': 'Whether or not to delete the messages of the banned user',
   'ban/reason': 'The reason for the ban',
+
   // Wikipedia
   wikipedia: 'Search Wikipedia for a term',
   'wikipedia/search': 'Term to search for',
+
   // Mute
   mute: 'Mutes a user from the server',
   'mute/user': 'The user to mute',
   'mute/time': 'The time for the mute',
   'mute/reason': 'The reason for the mute',
+
   // Kick
   kick: 'Kicks a user from the server',
   'kick/user': 'The user to kick',
   'kick/reason': 'The reason for the kick',
+
   // Animal
   animal: 'See a random image of an animal',
   'animal/panda': 'See a random image of a panda',
