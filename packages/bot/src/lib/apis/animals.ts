79ac5093677126708ec285b59fde4b38806e974a
Nicolas Ribeiro <nicolas.ribeiro.s@outlook.com>
2022-07-14 22:19:29 -0300

    refactor(animals): use type assertions

--- packages/bot/src/lib/apis/animals.ts
+++ packages/bot/src/lib/apis/animals.ts
@@ -8,51 +8,51 @@ export default class AnimalsAPI {
   }
 
   /** Get a random image of a cat */
-  async getRandomCat(): Promise<string> {
+  async getRandomCat() {
     const data = await this.request('https://aws.random.cat/meow').then(res => res.body.json());
 
-    return data.file;
+    return data.file as string;
   }
 
   /** Get a random image of a dog */
-  async getRandomDog(): Promise<string> {
+  async getRandomDog() {
     const data = await this.request('https://dog.ceo/api/breeds/image/random').then(res => res.body.json());
 
-    return data.message;
+    return data.message as string;
   }
 
   /** Get a random gif of a bunny */
-  async getRandomBunny(): Promise<string> {
+  async getRandomBunny() {
     const data = await this.request('https://api.bunnies.io/v2/loop/random/?media=gif,png').then(res => res.body.json());
 
-    return data.media.gif;
+    return data.media.gif as string;
   }
 
   /** Get a random image of a duck */
-  async getRandomDuck(): Promise<string> {
+  async getRandomDuck() {
     const data = await this.request('https://random-d.uk/api/v1/random?type=png').then(res => res.body.json());
 
-    return data.url;
+    return data.url as string;
   }
 
   /** Get a random image of a foxy */
-  async getRandomFoxy(): Promise<string> {
+  async getRandomFoxy() {
     const data = await this.request('https://randomfox.ca/floof/').then(res => res.body.json());
 
-    return data.image;
+    return data.image as string;
   }
 
   /** Get a random image of a koala */
-  async getRandomKoala(): Promise<string> {
+  async getRandomKoala() {
     const data = await this.request('https://some-random-api.ml/img/koala').then(res => res.body.json());
 
-    return data.link;
+    return data.link as string;
   }
 
   /** Get a random image of a panda */
-  async getRandomPanda(): Promise<string> {
+  async getRandomPanda() {
     const data = await this.request('https://some-random-api.ml/img/panda').then(res => res.body.json());
 
-    return data.link;
+    return data.link as string;
   }
 }
