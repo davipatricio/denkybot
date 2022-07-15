733f83ab2d86761712004b16380cf29fdc183e97
Nicolas Ribeiro <nicolas.ribeiro.s@outlook.com>
2022-07-15 16:47:30 -0300

    refactor: use ban archive to get sub commands

--- packages/bot/src/commands/Moderation/ban.ts
+++ packages/bot/src/commands/Moderation/ban.ts
@@ -2,7 +2,7 @@ import { PermissionFlagsBits } from 'discord.js';
 import { Command, CommandRunOptions } from '../../structures/Command';
 import type { DenkyClient } from '../../types/Client';
 
-export default class PingCommand extends Command {
+export default class BanCommand extends Command {
   constructor(client: DenkyClient) {
     super(client);
     this.rawName = 'BAN';
@@ -16,55 +16,18 @@ export default class PingCommand extends Command {
   }
 
   override run({ t, interaction }: CommandRunOptions) {
-    if (!interaction.inCachedGuild() || !interaction.guild.members.me) return;
+    switch (interaction.options.getSubcommand()) {
+      case 'user':
+        this.client.commands.get('_ban_user')?.run({ t, interaction });
+        break;
 
-    const deleteMessageDays = Number(interaction.options.getString('delete_messages') ?? 0);
-    const reason = interaction.options.getString('reason') ?? t('command:ban/no-reason');
+      case 'list':
+        this.client.commands.get('_ban_list')?.run({ t, interaction });
+        break;
 
-    const member = interaction.options.getMember('user');
-    const user = member?.user ?? interaction.options.getUser('user', true);
-
-    if (user.id === this.client.user?.id) {
-      interaction.editReply(`❌ ${interaction.user} **|** ${t('command:ban/error/ban-bot')}`);
-      return;
+      case 'info':
+        this.client.commands.get('_ban_info')?.run({ t, interaction });
+        break;
     }
-    if (user.id === interaction.user.id) {
-      interaction.editReply(`❌ ${interaction.user} **|** ${t('command:ban/error/ban-self')}`);
-      return;
-    }
-
-    if (member?.roles) {
-      if (!member.bannable || member.roles.highest.position >= interaction.guild.members.me.roles.highest.position) {
-        interaction.editReply(`❌ ${interaction.user} **|** ${t('command:ban/error/not-bannable')}`);
-        return;
-      }
-      if (interaction.member.roles.highest.position <= member.roles.highest.position) {
-        interaction.editReply(`❌ ${interaction.user} **|** ${t('command:ban/error/no-permissions')}`);
-        return;
-      }
-    }
-
-    interaction.guild.bans
-      .create(user.id, {
-        deleteMessageDays,
-        reason: `${t('command:ban/punished-by')} ${interaction.user.tag} - ${reason}`
-      })
-      .then(() => {
-        interaction.editReply(`✅ ${interaction.user} **|** ${t('command:ban/complete', user.tag ?? user.id ?? user)}`);
-      })
-      .catch(err => {
-        const error = err.toString().toLowerCase();
-        if (error.includes('missing permission')) {
-          return interaction.editReply(`❌ ${interaction.user} **|** ${t('command:ban/error/not-bannable')}`);
-        }
-        if (error.includes('unknown')) {
-          return interaction.editReply(`❌ ${interaction.user} **|** ${t('command:ban/error/unknown-member')}`);
-        }
-        if (error.includes('max')) {
-          return interaction.editReply(`❌ ${interaction.user} **|** ${t('command:ban/error/maximum-bans')}`);
-        }
-
-        return interaction.editReply(`❌ ${interaction.user} **|** ${t('command:ban/error/unknown-error')}`);
-      });
   }
 }
