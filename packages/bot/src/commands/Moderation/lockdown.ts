7666bb651ce2ad0029b7f253b42866cff13fcc7d
davipatricio <davipatricio2006@gmail.com>
2022-07-17 21:38:15 +0000

    feat(bot): base of enable and disable

--- packages/bot/src/commands/Moderation/lockdown.ts
+++ packages/bot/src/commands/Moderation/lockdown.ts
@@ -1,4 +1,5 @@
 import { PermissionFlagsBits } from 'discord.js';
+import ms from 'ms';
 import { Command, CommandRunOptions } from '../../structures/Command';
 import type { DenkyClient } from '../../types/Client';
 
@@ -15,7 +16,7 @@ export default class LockdownCommand extends Command {
     this.permissions = { bot: [PermissionFlagsBits.ManageChannels, PermissionFlagsBits.ManageGuild] };
   }
 
-  override run({ interaction }: CommandRunOptions) {
+  override run({ t, interaction }: CommandRunOptions) {
     if (interaction.guild!.memberCount < 30) {
       interaction.editReply(`❌ ${interaction.user} **|** O servidor possui menos de 30 membros, não é possível bloquear todos os canais.`);
       return;
@@ -23,19 +24,38 @@ export default class LockdownCommand extends Command {
 
     switch (interaction.options.getSubcommand(true)) {
       case 'enable':
-        this.#enableLockdown();
+        this.#enableLockdown({ t, interaction });
         break;
       case 'disable':
-        this.#disableLockdown();
+        this.#disableLockdown({ t, interaction });
         break;
     }
   }
 
-  #enableLockdown() {
-    return undefined;
+  async #enableLockdown({ interaction }: CommandRunOptions) {
+    const lockdown = await this.client.databases.getLockdown(interaction.guild!.id);
+    if (lockdown) {
+      interaction.editReply(`❌ ${interaction.user} **|** O servidor já está bloqueado.`);
+      return;
+    }
+
+    interaction.editReply(`✅ ${interaction.user} **|** O servidor foi bloqueado com sucesso.`);
+    await this.client.databases.createLockdown({
+      guildId: interaction.guild!.id,
+      startTime: BigInt(Date.now()),
+      blockedChannels: [interaction.channelId]
+    });
   }
 
-  #disableLockdown() {
-    return undefined;
+  async #disableLockdown({ interaction }: CommandRunOptions) {
+    const lockdown = await this.client.databases.getLockdown(interaction.guild!.id);
+    // Check if lockdown is 5 minutes old through the startTime property
+    const diff = BigInt(Date.now()) - BigInt(lockdown?.startTime ?? 0);
+    if (lockdown && diff < ms('5m')) {
+      interaction.editReply(`❌ ${interaction.user} **|** O servidor realizou um lockdown recentemente. Aguarde ${ms(diff)} ${ms(diff + ms('5m'))} e tente novamente.`);
+    }
+
+    interaction.editReply(`✅ ${interaction.user} **|** O servidor foi desbloqueado com sucesso.`);
+    await this.client.databases.deleteLockdown(interaction.guild!.id);
   }
 }
