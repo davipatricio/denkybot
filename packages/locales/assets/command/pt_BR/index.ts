e870ad649b53fba72a4720c12223ef77cf5716d2
codefactor-io <support@codefactor.io>
2022-07-19 01:22:28 +0000

    [CodeFactor] Apply fixes to commit 40075b1

--- packages/locales/assets/command/pt_BR/index.ts
+++ packages/locales/assets/command/pt_BR/index.ts
@@ -317,5 +317,5 @@ export default {
 
   'lockdown/sched/no-lockdown': 'Este servidor precisa ter um lockdown ativo para poder agendar um desbloqueio.',
   'lockdown/sched/deleted': 'Já havia um unlockdown programado para este servidor, então cancelamos o unlockdown.',
-  'lockdown/sched/done': 'Unlockdown programado com sucesso. Para cancelar, utilize `/lockdown agendar desbloqueio` novamente.',
+  'lockdown/sched/done': 'Unlockdown programado com sucesso. Para cancelar, utilize `/lockdown agendar desbloqueio` novamente.'
 } as const;
