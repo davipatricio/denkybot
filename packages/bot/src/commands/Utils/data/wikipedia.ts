01ac436aa6c2b932913d684e30389342a7fd38f3
davipatricio <davipatricio2006@gmail.com>
2022-07-29 18:08:00 +0000

    adapt wikipedia command

--- packages/bot/src/commands/Utils/data/wikipedia.ts
+++ packages/bot/src/commands/Utils/data/wikipedia.ts
@@ -1,23 +1,22 @@
 import { CommandDataStructure } from '#structures/CommandDataStructure';
 import type { DenkyClient } from '#types/Client';
-import { ApplicationCommandOptionType } from 'discord.js';
+import { SlashCommandStringOption } from 'discord.js';
 
 export default class WikipediaData extends CommandDataStructure {
   constructor(client: DenkyClient) {
     super(client);
-    this.t('a');
-    // this.parseData(client, {
-    //   this.setName('wikipedia',
-    //   .setDMPermission(true)
-    //   .setDescription('wikipedia',
-    //   options: [
-    //     {
-    //       this.setName('wikipedia/search',
-    //       .setDescription('wikipedia/search',
-    //       required: true,
-    //       type: ApplicationCommandOptionType.String
-    //     }
-    //   ]
-    // });
+    this.setName('wikipedia')
+      .setDMPermission(true)
+      .setDescription(this.t('en_US', 'commandDescriptions:wikipedia'))
+      .setDescriptionLocalization('pt-BR', this.t('pt_BR', 'commandDescriptions:wikipedia'))
+      .addStringOption(
+        new SlashCommandStringOption()
+          .setName(this.t('en_US', 'commandNames:wikipedia/search'))
+          .setNameLocalization('pt-BR', this.t('pt_BR', 'commandNames:wikipedia/search'))
+          .setDescription(this.t('en_US', 'commandDescriptions:wikipedia/search'))
+          .setDescriptionLocalization('pt-BR', this.t('pt_BR', 'commandDescriptions:wikipedia/search'))
+          .setMaxLength(100)
+          .setRequired(true)
+      );
   }
 }
