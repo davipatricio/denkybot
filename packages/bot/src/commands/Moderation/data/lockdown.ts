998883c4e6c91a7e507356fa171a12beb9d37d11
davipatricio <davipatricio2006@gmail.com>
2022-07-31 17:25:38 +0000

    update lockdown

--- packages/bot/src/commands/Moderation/data/lockdown.ts
+++ packages/bot/src/commands/Moderation/data/lockdown.ts
@@ -1,49 +1,53 @@
 import { CommandDataStructure } from '#structures/CommandDataStructure';
 import type { DenkyClient } from '#types/Client';
-import { ApplicationCommandOptionType, PermissionFlagsBits } from 'discord.js';
+import { PermissionFlagsBits, PermissionsBitField, SlashCommandStringOption, SlashCommandSubcommandBuilder, SlashCommandSubcommandGroupBuilder } from 'discord.js';
 
 export default class LockdownData extends CommandDataStructure {
   constructor(client: DenkyClient) {
     super(client);
 
-    
-      this.setName('lockdown',
-      .setDMPermission(false),
-      defaultMemberPermissions: [PermissionFlagsBits.ManageChannels, PermissionFlagsBits.ManageGuild],
-      .setDescription('lockdown',
-      options: [
-        {
-          this.setName('lockdown/enable',
-          .setDescription('lockdown/enable',
-          type: ApplicationCommandOptionType.Subcommand
-        },
-        {
-          this.setName('lockdown/disable',
-          .setDescription('lockdown/disable',
-          type: ApplicationCommandOptionType.Subcommand
-        },
-        {
-          this.setName('lockdown/schedule',
-          .setDescription('lockdown/schedule',
-          type: ApplicationCommandOptionType.SubcommandGroup,
-          options: [
-            {
-              this.setName('lockdown/schedule/unlockdown',
-              .setDescription('lockdown/schedule',
-              type: ApplicationCommandOptionType.Subcommand,
-              options: [
-                {
-                  this.setName('lockdown/schedule/unlockdown/duration',
-                  .setDescription('lockdown/schedule/unlockdown/duration',
-                  type: ApplicationCommandOptionType.String,
-                  required: true,
-                  minLength: 2,
-                  maxLength: 15
-                }
-              ]
-            }
-          ]
-        }
-      ]
+    this.setName('lockdown')
+      .setDMPermission(false)
+      .setDefaultMemberPermissions(new PermissionsBitField().add([PermissionFlagsBits.ManageChannels, PermissionFlagsBits.ManageGuild]).bitfield)
+      .setDescription(this.t('commandDescriptions:lockdown'))
+      .setDescriptionLocalizations(this.localizations('commandDescriptions:lockdown'))
+      .addSubcommand(
+        new SlashCommandSubcommandBuilder()
+          .setName(this.t('commandNames:lockdown/enable'))
+          .setNameLocalizations(this.localizations('commandNames:lockdown/enable'))
+          .setDescription(this.t('commandDescriptions:lockdown/enable'))
+          .setDescriptionLocalizations(this.localizations('commandDescriptions:lockdown/enable'))
+      )
+      .addSubcommand(
+        new SlashCommandSubcommandBuilder()
+          .setName(this.t('commandNames:lockdown/disable'))
+          .setNameLocalizations(this.localizations('commandNames:lockdown/disable'))
+          .setDescription(this.t('commandDescriptions:lockdown/disable'))
+          .setDescriptionLocalizations(this.localizations('commandDescriptions:lockdown/disable'))
+      )
+      .addSubcommandGroup(
+        new SlashCommandSubcommandGroupBuilder()
+          .setName(this.t('commandNames:lockdown/schedule'))
+          .setNameLocalizations(this.localizations('commandNames:lockdown/schedule'))
+          .setDescription(this.t('commandDescriptions:lockdown/schedule'))
+          .setDescriptionLocalizations(this.localizations('commandDescriptions:lockdown/schedule'))
+          .addSubcommand(
+            new SlashCommandSubcommandBuilder()
+              .setName(this.t('commandNames:lockdown/schedule/unlockdown'))
+              .setNameLocalizations(this.localizations('commandNames:lockdown/schedule/unlockdown'))
+              .setDescription(this.t('commandDescriptions:lockdown/schedule/unlockdown'))
+              .setDescriptionLocalizations(this.localizations('commandDescriptions:lockdown/schedule/unlockdown'))
+              .addStringOption(
+                new SlashCommandStringOption()
+                  .setName(this.t('commandNames:lockdown/schedule/unlockdown/duration'))
+                  .setNameLocalizations(this.localizations('commandNames:lockdown/schedule/unlockdown/duration'))
+                  .setDescription(this.t('commandDescriptions:lockdown/schedule/unlockdown/duration'))
+                  .setDescriptionLocalizations(this.localizations('commandDescriptions:lockdown/schedule/unlockdown/duration'))
+                  .setRequired(true)
+                  .setMinLength(2)
+                  .setMaxLength(15)
+              )
+          )
+      );
   }
 }
