79e4197db15ab0d2f71ceff9166e5f4f52471861
davipatricio <davipatricio2006@gmail.com>
2022-08-01 18:58:58 +0000

    update giveaway

--- packages/bot/src/commands/Moderation/data/giveaway.ts
+++ packages/bot/src/commands/Moderation/data/giveaway.ts
@@ -1,76 +1,85 @@
 import { CommandDataStructure } from '@bot/src/structures/CommandDataStructure';
 import type { DenkyClient } from '@bot/src/types/Client';
-import { ApplicationCommandOptionType, ChannelType, PermissionFlagsBits } from 'discord.js';
+import { ChannelType, PermissionFlagsBits, SlashCommandChannelOption, SlashCommandNumberOption, SlashCommandStringOption, SlashCommandSubcommandBuilder } from 'discord.js';
 
 export default class GiveawayData extends CommandDataStructure {
   constructor(client: DenkyClient) {
     super(client);
 
-    
-      this.setName('giveaway',
-      .setDMPermission(false),
-      defaultMemberPermissions: [PermissionFlagsBits.ManageGuild],
-      .setDescription('giveaway',
-      options: [
-        {
-          this.setName('giveaway/create',
-          .setDescription('giveaway/create',
-          type: ApplicationCommandOptionType.Subcommand,
-          options: [
-            {
-              this.setName('giveaway/create/title',
-              .setDescription('giveaway/create/title',
-              type: ApplicationCommandOptionType.String,
-              required: true,
-              maxLength: 40
-            },
-            {
-              this.setName('giveaway/create/winners',
-              .setDescription('giveaway/create/winners',
-              type: ApplicationCommandOptionType.Number,
-              required: true,
-              minValue: 1,
-              maxValue: 100
-            },
-            {
-              this.setName('giveaway/create/duration',
-              .setDescription('giveaway/create/duration',
-              type: ApplicationCommandOptionType.String,
-              required: true,
-              minValue: 2,
-              maxValue: 15
-            },
-            {
-              this.setName('giveaway/create/description',
-              .setDescription('giveaway/create/description',
-              type: ApplicationCommandOptionType.String,
-              required: false,
-              maxLength: 300
-            },
-            {
-              this.setName('giveaway/create/channel',
-              .setDescription('giveaway/create/channel',
-              type: ApplicationCommandOptionType.Channel,
-              required: false,
-              channelTypes: [ChannelType.GuildText, ChannelType.GuildNews, ChannelType.GuildVoice, ChannelType.GuildNewsThread, ChannelType.GuildPrivateThread, ChannelType.GuildPublicThread]
-            }
-          ]
-        },
-        {
-          this.setName('giveaway/end',
-          .setDescription('giveaway/end',
-          type: ApplicationCommandOptionType.Subcommand,
-          options: [
-            {
-              this.setName('giveaway/end/id',
-              .setDescription('giveaway/end/id',
-              required: true,
-              type: ApplicationCommandOptionType.String,
-              minLength: 19,
-              maxLength: 21
-            }
-          ]
-        }
-      ]
+    this.setName(this.t('commandNames:giveaway'))
+      .setNameLocalizations(this.localizations('commandNames:giveaway'))
+      .setDMPermission(false)
+      .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+      .setDescription('commandDescriptions:giveaway')
+      .setDescriptionLocalizations(this.localizations('commandDescriptions:giveaway'))
+      .addSubcommand(
+        new SlashCommandSubcommandBuilder()
+          .setName(this.t('commandNames:giveaway/create'))
+          .setNameLocalizations(this.localizations('commandNames:giveaway/create'))
+          .setDescription(this.t('commandDescriptions:giveaway/create'))
+          .setDescriptionLocalizations(this.localizations('commandDescriptions:giveaway/create'))
+          .addStringOption(
+            new SlashCommandStringOption()
+              .setName(this.t('commandNames:giveaway/create/title'))
+              .setNameLocalizations(this.localizations('commandNames:giveaway/create/title'))
+              .setDescription(this.t('commandDescriptions:giveaway/create/title'))
+              .setDescriptionLocalizations(this.localizations('commandDescriptions:giveaway/create/title'))
+              .setRequired(true)
+              .setMaxLength(40)
+          )
+          .addNumberOption(
+            new SlashCommandNumberOption()
+              .setName(this.t('commandNames:giveaway/create/winners'))
+              .setNameLocalizations(this.localizations('commandNames:giveaway/create/winners'))
+              .setDescription(this.t('commandDescriptions:giveaway/create/winners'))
+              .setDescriptionLocalizations(this.localizations('commandDescriptions:giveaway/create/winners'))
+              .setRequired(true)
+              .setMinValue(1)
+              .setMaxValue(100)
+          )
+          .addNumberOption(
+            new SlashCommandNumberOption()
+              .setName(this.t('commandNames:giveaway/create/duration'))
+              .setNameLocalizations(this.localizations('commandNames:giveaway/create/duration'))
+              .setDescription(this.t('commandDescriptions:giveaway/create/duration'))
+              .setDescriptionLocalizations(this.localizations('commandDescriptions:giveaway/create/duration'))
+              .setRequired(true)
+              .setMinValue(2)
+              .setMaxValue(15)
+          )
+          .addStringOption(
+            new SlashCommandStringOption()
+              .setName(this.t('commandNames:giveaway/create/description'))
+              .setNameLocalizations(this.localizations('commandNames:giveaway/create/description'))
+              .setDescription(this.t('commandDescriptions:giveaway/create/description'))
+              .setDescriptionLocalizations(this.localizations('commandDescriptions:giveaway/create/description'))
+              .setMaxLength(300)
+          )
+          .addChannelOption(
+            new SlashCommandChannelOption()
+              .setName(this.t('commandNames:giveaway/create/channel'))
+              .setNameLocalizations(this.localizations('commandNames:giveaway/create/channel'))
+              .setDescription(this.t('commandDescriptions:giveaway/create/channel'))
+              .setDescriptionLocalizations(this.localizations('commandDescriptions:giveaway/create/channel'))
+              .addChannelTypes(ChannelType.GuildText, ChannelType.GuildNews, ChannelType.GuildVoice, ChannelType.GuildNewsThread, ChannelType.GuildPrivateThread, ChannelType.GuildPublicThread)
+          )
+      )
+      .addSubcommand(
+        new SlashCommandSubcommandBuilder()
+          .setName(this.t('commandNames:giveaway/end'))
+          .setNameLocalizations(this.localizations('commandNames:giveaway/end'))
+          .setDescription(this.t('commandDescriptions:giveaway/end'))
+          .setDescriptionLocalizations(this.localizations('commandDescriptions:giveaway/end'))
+          .addStringOption(
+            new SlashCommandStringOption()
+              .setName(this.t('commandNames:giveaway/end/id'))
+              .setNameLocalizations(this.localizations('commandNames:giveaway/end/id'))
+              .setDescription(this.t('commandDescriptions:giveaway/end/id'))
+              .setDescriptionLocalizations(this.localizations('commandDescriptions:giveaway/end/id'))
+              .setRequired(true)
+              .setMinLength(19)
+              .setMaxLength(21)
+          )
+      );
   }
 }
