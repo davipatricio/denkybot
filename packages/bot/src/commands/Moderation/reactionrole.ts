bce3b369e7ddb841bd5ad615567ca074aa00c20f
davipatricio <davipatricio@pm.me>
2022-08-23 18:41:51 -0300

    add checks & add reaction role creation code

--- packages/bot/src/commands/Moderation/reactionrole.ts
+++ packages/bot/src/commands/Moderation/reactionrole.ts
@@ -30,8 +30,9 @@ export default class ReactionRoleCommand extends Command {
   }
 
   async #addReactionRole({ t, interaction }: CommandRunOptions) {
-    // const actionType = interaction.options.getString('type', true) as keyof typeof ReactionRoleType;
+    const actionType = interaction.options.getString('type', true) as keyof typeof ReactionRoleType;
     const role = interaction.options.getRole('role', true);
+    const messageId = interaction.options.getString('message_id', true);
     if (role.managed || role.id === interaction.guild!.id) {
       interaction.editReply(`❌ ${interaction.user} **|** ${t('command:buttonroles/managed-role')}.`);
       return;
@@ -42,15 +43,44 @@ export default class ReactionRoleCommand extends Command {
       return;
     }
 
-    const message = await interaction.editReply(`➕ ${interaction.user} **|** Certo. Agora, escolha um emoji para representar este cargo. Para fazer isso, adicione uma reação nesta mensagem.`);
-    const collector = message.createReactionCollector({
+    const finalMessage = await interaction.channel!.messages.fetch(messageId).catch(() => null);
+    if (!finalMessage) {
+      interaction.editReply(`❌ ${interaction.user} **|** Mensagem inexistente neste canal.`);
+      return;
+    }
+
+    const message = await interaction.editReply(`➕ ${interaction.user} **|** Certo. Agora, escolha um emoji para representar este cargo. Para fazer isso, adicione uma reação na mensagem escolhida.`);
+    const collector = finalMessage.createReactionCollector({
       time: 60000,
       max: 1,
       filter: (_, user) => user.id === interaction.user.id
     });
 
-    collector.on('collect', (reaction, user) => {
-      reaction.users.remove(user);
+    collector.on('collect', async reaction => {
+      await finalMessage.react(reaction.emoji.identifier);
+
+      const alreadyExists = await this.client.databases.reactionRole
+        .findFirstOrThrow({
+          where: {
+            emojiId: reaction.emoji.identifier,
+            messageId: finalMessage.id
+          }
+        })
+        .then(() => true)
+        .catch(() => false);
+      if (alreadyExists) {
+        interaction.editReply(`❌ ${interaction.user} **|** Este emoji já está associado a outro cargo na mensagem escolhida.`);
+        return;
+      }
+
+      await this.client.databases.reactionRole.create({
+        data: {
+          emojiId: reaction.emoji.identifier,
+          messageId: finalMessage.id,
+          type: ReactionRoleType[actionType]
+        }
+      });
+      interaction.editReply(`✅ ${interaction.user} **|** Criado com sucesso.`);
     });
 
     collector.on('end', collected => {
