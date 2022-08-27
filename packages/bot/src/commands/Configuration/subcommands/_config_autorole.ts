ba218e737ffaf867f29d91c475b401c4d41d1034
davipatricio <davipatricio@pm.me>
2022-08-26 23:41:29 -0300

    add more code

--- packages/bot/src/commands/Configuration/subcommands/_config_autorole.ts
+++ packages/bot/src/commands/Configuration/subcommands/_config_autorole.ts
@@ -1,8 +1,45 @@
-import type { CommandRunOptions } from '@bot/src/structures/Command';
+import type { CommandLocale, CommandRunOptions } from '@bot/src/structures/Command';
 import { SubCommand } from '@bot/src/structures/SubCommand';
+import type { AutoRole } from '@prisma/client';
+import { ActionRowBuilder, ChatInputCommandInteraction, Colors, EmbedBuilder, Message, SelectMenuBuilder } from 'discord.js';
+
+type PageTypes = 'autorole' | 'roles' | 'delay' | 'bots';
 
 export default class AutoRoleSubCommand extends SubCommand {
-  run({ t, interaction }: CommandRunOptions) {
-    return { t, interaction };
+  async run({ t, interaction }: CommandRunOptions) {
+    if (!interaction.inCachedGuild()) return;
+    const configStatus = (await this.client.databases.getAutoRole(interaction.guild.id)) ?? undefined;
+
+    const selectRow = new ActionRowBuilder<SelectMenuBuilder>();
+    const { embed /* , buttonRow */ } = this.updateMessage('autorole', null, selectRow, interaction, configStatus, t);
+
+    await interaction.editReply({
+      components: [selectRow /* , buttonRow */],
+      embeds: [embed]
+    });
   }
+
+  updateMessage(page: PageTypes, message: Message | null, selectRow: ActionRowBuilder<SelectMenuBuilder>, interaction: ChatInputCommandInteraction, config: AutoRole | undefined, t: CommandLocale) {
+    const embed = this.generateEmbedPage(page, interaction, config, t);
+    // const buttonRow = this.generateButtonsFromPage(page, config, t);
+    message?.edit({ components: [selectRow /* buttonRow */], embeds: [embed] });
+    return { embed /* , buttonRow */ };
+  }
+
+  generateEmbedPage(page: PageTypes, { user }: ChatInputCommandInteraction, configStatus: AutoRole | undefined, t: CommandLocale) {
+    const embed = new EmbedBuilder({
+      timestamp: Date.now(),
+      footer: { text: user.tag, iconURL: user.displayAvatarURL() }
+    })
+      .setColor(Colors.Blurple)
+      .setTitle('🔧 AutoRole');
+    switch (page) {
+      case 'autorole':
+        return embed.setDescription(configStatus ? `✅ **|** ${t('command:config/suggestions/enabled')}` : `❌ **|** ${t('command:config/suggestions/disabled')}`);
+      default:
+        return embed;
+    }
+  }
+
+  // generateButtonsFromPage(page: PageTypes, configStatus: AutoRole | undefined, t: CommandLocale) {}
 }
